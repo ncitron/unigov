7761d8263236f11c5e7428e788718e88772ea4d6
Noah Citron <noah@jeff.org>
2020-10-19 04:23:30 -0400

    added delegate button (nonfunctional)

--- src/components/Delegate.js
+++ src/components/Delegate.js
@@ -2,10 +2,15 @@ import React from 'react';
 class Delegate extends React.Component {
     render() {
         return (
-            <div className='Delegate'>
-                {this.props.delegate.delegate}
-                <br />
-                votes: {this.props.delegate.votes} UNI
+            <div className='Delegate row'>
+                <div class="col-9">
+                    {this.props.delegate.delegate}
+                    <br />
+                    votes: {this.props.delegate.votes} UNI
+                </div>
+                <div class="col-3 delegate-button-container">
+                    <button type="button" class="btn btn-primary">Delegate</button>
+                </div>
             </div>
         );
     }
