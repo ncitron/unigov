e7b8c24df8dcee3cccfed0537b24f08a96f5ee2d
Noah Citron <noah@jeff.org>
2020-10-20 02:15:00 -0400

    added working navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,95 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
 import './App.css';
+import Web3 from 'web3'
+import { abi as uniABI } from './abi/uni.json';
 import DelegateList from './components/DelegateList';
 
-function App() {
-  return (
-    <div className="App">
-        <div class="row header">
-            <div class="col-3 name-page">Uniswap Governance</div>
-            <div class="col-6 nav">
-                <div class="delegate-nav">Delegates</div>
-                <div class="ap-nav">Autonomous Proposals</div>
+class App extends React.Component {
+    state = {
+        delegates: []
+    }
+
+    async componentDidMount() {
+        const web3 = new Web3(window.ethereum);
+        const uniAddress = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984';
+        let uni = new web3.eth.Contract(uniABI, uniAddress);
+
+        let delegations = []
+        for(let i = 10_750_000; i < await web3.eth.getBlockNumber(); i+=100_000) {
+            console.log('working');
+            delegations.push(...await uni.getPastEvents('DelegateVotesChanged', {
+                fromBlock: (i-100_000),
+                toBlock: i
+            }));
+        }
+        const delegateAccounts = {};
+
+        delegations.forEach(e => {
+            const { delegate, newBalance } = e.returnValues;
+            delegateAccounts[delegate] = newBalance;
+        });
+
+        let delegates = [];
+        Object.keys(delegateAccounts).forEach((account) => {
+            const voteWeight = +delegateAccounts[account];
+            if (voteWeight === 0) return;
+            delegates.push({
+            delegate: account,
+            votes: voteWeight
+            });
+        });
+
+        delegates = delegates.sort((a, b) => {
+            return a.votes < b.votes ? 1 : -1;
+        });
+
+        delegates = delegates.slice(0, 50);
+
+        for(let i = 0; i < delegates.length; i++) {
+            delegates[i].votes = parseInt(await uni.methods.getCurrentVotes(delegates[i].delegate).call() / 1e18);
+            console.log('sorting');
+        }
+
+        delegates = delegates.sort((a, b) => {
+            return a.votes < b.votes ? 1 : -1;
+        });
+
+        this.setState({delegates: delegates.slice(0, 20)});
+
+
+        this.forceUpdate();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Router>
+                    <div class="row header">
+                        <div class="col-4 name-page">Uniswap Governance</div>
+                        <div class="col-4 nav">
+                            <NavLink to='/ap' className="ap-nav" style={{ textDecoration: 'none'}} activeStyle = {{ color: '#db2cc4', borderBottom: '2px solid #db2cc4'}}>
+                                    Autonomous Proposals
+                            </NavLink>
+                            <NavLink to='/del' className="delegate-nav" 
+                            style={{ textDecoration: 'none'}} 
+                            activeStyle = {{ color: '#db2cc4', borderBottom: '2px solid #db2cc4'}}>
+                                    Delegates
+                            </NavLink>
+                        </div>
+                        <div class="col-4"></div>
+                    </div>
+                    <div class="hero" />
+                    <Switch>
+                        <Route path='/del'>
+                            <DelegateList delegates={this.state.delegates} />
+                        </Route>
+                        <Route path='/ap'></Route>
+                    </Switch>
+                </Router>
             </div>
-            <div class="col-3"></div>
-        </div>
-        <div class="hero" />
-        <DelegateList />
-    </div>
-  );
+        );
+    }
 }
 
 export default App;
--- src/components/DelegateList.js
+++ src/components/DelegateList.js
@@ -1,29 +1,25 @@
 import React from 'react';
-import Web3 from 'web3'
 import { css } from "@emotion/core";
-import ClipLoader from "react-spinners/ClipLoader";
-import { abi as uniABI } from '../abi/uni.json';
 import Delegate from './Delegate.js';
-import { BeatLoader } from 'react-spinners';
 
 
 class DelegateList extends React.Component {
-
-    state = {
-        delegates: []
-    }
-
     render() {
+        const override = css`
+            display: block;
+            margin: 0 auto;
+            border-color: red;
+            `;
         let loader;
-        if(this.state.delegates.length === 0) {
-            loader = 
-                <div className="sweet-loading">
-                    <BeatLoader
-                    size={30}
-                    color={"#ed1f89"}
-                    loading={this.state.loading}
-                    />
+        if(this.props.delegates.length === 0) {
+            loader =
+            <div class="spinner">
+                <div class="d-flex justify-content-center">
+                    <div class="spinner-border" role="status">
+                        <span class="sr-only">Loading...</span>
+                    </div>
                 </div>
+            </div>
         }
 
         return (
@@ -31,9 +27,9 @@ class DelegateList extends React.Component {
                 <div class="row">
                     <div class="col-3" />
                     <div class="col-6 delegate-list">
-                        {loader}
                         <div class="delegate-title">Delegates</div>
-                        {this.state.delegates.map(delegate => (
+                        {loader}
+                        {this.props.delegates.map(delegate => (
                             <Delegate delegate={delegate} />
                         ))}
                     </div>
@@ -41,48 +37,6 @@ class DelegateList extends React.Component {
                 </div>
             </div>
         );
-    }
-
-    async componentDidMount() {
-        const web3 = new Web3(window.ethereum);
-        const uniAddress = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984';
-        let uni = new web3.eth.Contract(uniABI, uniAddress);
-
-        let delegations = []
-        for(let i = 10_500_000; i < await web3.eth.getBlockNumber(); i+=50_000) {
-            console.log('working');
-            delegations.push(...await uni.getPastEvents('DelegateVotesChanged', {
-                fromBlock: (i-100_000),
-                toBlock: i
-            }));
-        }
-        const delegateAccounts = {};
-
-        delegations.forEach(e => {
-            const { delegate, newBalance } = e.returnValues;
-            delegateAccounts[delegate] = newBalance;
-        });
-
-        const delegates = [];
-        Object.keys(delegateAccounts).forEach((account) => {
-            const voteWeight = +delegateAccounts[account];
-            if (voteWeight === 0) return;
-            delegates.push({
-            delegate: account,
-            votes: voteWeight
-            });
-        });
-
-        delegates.sort((a, b) => {
-            return a.votes < b.votes ? 1 : -1;
-        });
-
-        delegates.forEach(d => {
-            d.votes = (d.votes / 1e18).toFixed(0);
-        });
-
-        this.state.delegates = delegates.slice(0, 20);
-        this.forceUpdate();
     }
 }
 
