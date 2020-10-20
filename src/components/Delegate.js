603cd500a630069c29551b60c4aaa5664cb2ca9c
Noah Citron <noah@jeff.org>
2020-10-20 02:52:26 -0400

    delegation now works

--- src/components/Delegate.js
+++ src/components/Delegate.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import Web3 from 'web3'
+import { abi as uniABI } from '../abi/uni.json';
+
 class Delegate extends React.Component {
+
+    state = {
+        uni: {},
+        account: ''
+    }
+
+    async componentDidMount() {
+        const web3 =new Web3(Web3.givenProvider);
+        const uniAddress = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984';
+        this.setState({ uni: new web3.eth.Contract(uniABI, uniAddress) });
+        this.setState({ account: (await web3.eth.requestAccounts())[0]})
+        
+    }
+
+    delegateTo = async () => {
+        console.log('delegating')
+        console.log(this.state.account);
+        await this.state.uni.methods.delegate(this.props.delegate.delegate).send({ from: this.state.account })
+    }
+
     render() {
         return (
             <div className='Delegate row'>
@@ -9,7 +32,7 @@ class Delegate extends React.Component {
                     votes: {this.props.delegate.votes} UNI
                 </div>
                 <div class="col-3 delegate-button-container">
-                    <button type="button" class="btn btn-primary">Delegate</button>
+                    <button type="button" class="btn btn-primary" onClick={this.delegateTo}>Delegate</button>
                 </div>
             </div>
         );
